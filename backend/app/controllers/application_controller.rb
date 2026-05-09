640ad6f38fac6cb1a8275341fc5569c2bdde28ec
Filipe Bezerra <bezerrafilipe13@gmail.com>
2026-05-09 18:44:14 -0300

    feat(backend): add CORS, routes and authentication middleware

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  private
+
+  def current_user
+    header = request.headers['Authorization']
+    token = header.split(' ').last if header
+    User.find_by(auth_token: token) if token
+  end
+
+  def authenticate_user!
+    unless current_user
+      render json: { error: 'Unauthorized' }, status: :unauthorized
+    end
+  end
 end
