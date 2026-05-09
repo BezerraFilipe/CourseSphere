cd1bbfe661fdff7a89b5c8f3aa1e335cd78ec99f
Filipe Bezerra <bezerrafilipe13@gmail.com>
2026-05-09 11:22:47 -0300

    feat(backend): add User model with validations and auth token

--- backend/db/migrate/20260507131346_create_users.rb
+++ backend/db/migrate/20260507131346_create_users.rb
@@ -6,9 +6,10 @@ class CreateUsers < ActiveRecord::Migration[8.1]
       t.string :password_digest,      null: false
       t.string :auth_token
 
-      add_index :users, :email,       unique: true
-      add_index :users, :auth_token,  unique: true
       t.timestamps
     end
+
+    add_index :users, :email,       unique: true
+    add_index :users, :auth_token,  unique: true
   end
 end
