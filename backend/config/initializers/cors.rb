61f8d461592424d0cd9bdd99d2d2eaa6e2f8904e
Filipe Bezerra <bezerrafilipe13@gmail.com>
2026-05-10 18:17:40 -0300

    fix(backend): update CORS with production Vercel URL

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173", ENV.fetch("FRONTEND_URL", "")
+    origins "http://localhost:5173", ENV.fetch("FRONTEND_URL", "https://coursesphere-front.vercel.app")
 
     resource "*",
       headers: :any,
