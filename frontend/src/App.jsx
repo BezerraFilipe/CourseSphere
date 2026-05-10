f199b9ffb41458f6f70602ff446a9e1891b7bb97
Filipe Bezerra <bezerrafilipe13@gmail.com>
2026-05-09 23:22:24 -0300

    chore(frontend): update base styles and routes with PublicRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import PrivateRoute from './components/PrivateRoute'
+import PublicRoute from './components/PublicRoute'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -14,21 +15,13 @@ export default function App() {
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={
-            <PrivateRoute><Dashboard /></PrivateRoute>
-          } />
-          <Route path="/my-courses" element={
-            <PrivateRoute><MyCourses /></PrivateRoute>
-          } />
-          <Route path="/courses/new" element={
-            <PrivateRoute><CourseForm /></PrivateRoute>
-          } />
-          <Route path="/courses/:id/edit" element={
-            <PrivateRoute><CourseForm /></PrivateRoute>
-          } />
+          <Route path="/" element={<PublicRoute><Home /></PublicRoute>} />
+          <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+          <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
+          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          <Route path="/my-courses" element={<PrivateRoute><MyCourses /></PrivateRoute>} />
+          <Route path="/courses/new" element={<PrivateRoute><CourseForm /></PrivateRoute>} />
+          <Route path="/courses/:id/edit" element={<PrivateRoute><CourseForm /></PrivateRoute>} />
         </Routes>
       </AuthProvider>
     </BrowserRouter>
