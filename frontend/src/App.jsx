6689d764fc408417b36708d1dd470063f92c0d27
Filipe Bezerra <bezerrafilipe13@gmail.com>
2026-05-09 23:46:33 -0300

    feat(frontend): wrap app with ToastProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext'
 import PrivateRoute from './components/PrivateRoute'
 import PublicRoute from './components/PublicRoute'
+import { ToastProvider } from './contexts/ToastContext'
 
 import Home from './pages/Home'
 import Login from './pages/Login'
@@ -14,15 +15,17 @@ export default function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <Routes>
-          <Route path="/" element={<PublicRoute><Home /></PublicRoute>} />
-          <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
-          <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
-          <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-          <Route path="/my-courses" element={<PrivateRoute><MyCourses /></PrivateRoute>} />
-          <Route path="/courses/new" element={<PrivateRoute><CourseForm /></PrivateRoute>} />
-          <Route path="/courses/:id/edit" element={<PrivateRoute><CourseForm /></PrivateRoute>} />
-        </Routes>
+        <ToastProvider> 
+          <Routes>
+            <Route path="/" element={<PublicRoute><Home /></PublicRoute>} />
+            <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+            <Route path="/register" element={<PublicRoute><Register /></PublicRoute>} />
+            <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+            <Route path="/my-courses" element={<PrivateRoute><MyCourses /></PrivateRoute>} />
+            <Route path="/courses/new" element={<PrivateRoute><CourseForm /></PrivateRoute>} />
+            <Route path="/courses/:id/edit" element={<PrivateRoute><CourseForm /></PrivateRoute>} />
+          </Routes>
+        </ToastProvider>
       </AuthProvider>
     </BrowserRouter>
   )
